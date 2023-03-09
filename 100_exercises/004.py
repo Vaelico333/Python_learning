e3ce64470790ff71918b3165dbfe8472a0688e65
Vaelico333 <107014096+Vaelico333@users.noreply.github.com>
2023-03-09 03:52:42 +0100

    Day 1 of the challenge

---
+++ 100_exercises/004.py
@@ -0,0 +1,11 @@
+# Question: Fix the last line so that it outputs the sum of 1 and 2. Please do not change the first two lines. Only the last one.
+
+"""a = "1"
+b = 2
+print(a + b)"""
+
+# My solution:
+
+a = "1"
+b = 2
+print(int(a) + b)
---
+++ 100_exercises/005.py
@@ -0,0 +1,5 @@
+# Question: Complete the script so that it prints out the second item of the list.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# My answer:
+print(letters[1])
